b9312bac18f32dcb586aca826d615bab9b820704
Pavlin Georgiev <pavlingeorgiev2002@gmail.com>
2022-01-11 10:57:14 +0200

    Add main file which operates with print functions and builds the console app

---
+++ matrixCalculator.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "calculatingFunctions.h"
+#include "printFunctions.h"
+using namespace std;
+
+
+int main()
+{
+    int operation = 0;
+    while (true)
+    {
+        if (operation != 0 && operation != 8)
+        {
+            continueMenu();
+            cin >> operation;
+            if (operation == 1)
+            {
+                clearScreen();
+                operation = 0;
+            }
+            if (operation == 2)
+            {
+                clearScreen();
+                cout << "Goodbye!";
+                break;
+            }
+        }
+        if (operation == 0)
+        {
+            startMenu();
+            cin >> operation;
+        }
+        if (operation == 8)
+        {
+            clearScreen();
+            cout << "Goodbye!";
+            break;            
+        }
+        if (operation == 1)
+        {
+            additionPrint();
+        }
+        if (operation == 2)
+        {
+            multiplicationWithNumberPrint();
+        }
+        if (operation == 3)
+        {
+            multiplicationPrint();
+        }
+        if (operation == 4)
+        {
+            devisionWithNumberPrint();
+        }
+        if (operation == 5)
+        {
+            determinantPrint();
+        }
+        if (operation == 6)
+        {
+            transposePrint();
+        }
+        if (operation == 7)
+        {
+            inversePrint();
+        }
+    }
+    return 0;
+}
